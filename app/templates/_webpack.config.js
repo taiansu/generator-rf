306be93e687942fe48552a5429047f37dbff4f83
Tom Chen <developer@tomchentw.com>
2015-01-16 23:26:52 -0800

    style(_webpack.config.js): indention using two spaces

--- app/templates/_webpack.config.js
+++ app/templates/_webpack.config.js
@@ -1,49 +1,48 @@
- module.exports = {
-    entry: {
-      app: ["webpack/hot/dev-server", "./src/scripts/main<%= scriptSuffix %>"]
-    },
-    output: {
-      path: "./build",
-      filename: "bundle.js"
-    },
-    plugins: [],
-    resolve: {
-      modulesDirectories: ['node_modules'],
-    },
-    module: {
-        loaders: [
-            <% if ( dialect == 'LiveScript') { %>
-            { test: /\.ls$/, loader: "livescript!cjsx" },
-            <% } else if ( dialect =='JavaScript') { %>
-            { test: /\.jsx?$/, loader: "jsx" },
-            <% } else { %>
-            { test: /\.coffee$/, loader: "coffee!cjsx" },
-            <% } %>
+module.exports = {
+  entry: {
+    app: ["webpack/hot/dev-server", "./src/scripts/main<%= scriptSuffix %>"]
+  },
+  output: {
+    path: "./build",
+    filename: "bundle.js"
+  },
+  plugins: [],
+  resolve: {
+    modulesDirectories: ['node_modules'],
+  },
+  module: {
+    loaders: [
+      <% if ( dialect == 'LiveScript') { %>
+      { test: /\.ls$/, loader: "livescript!cjsx" },
+      <% } else if ( dialect =='JavaScript') { %>
+      { test: /\.jsx?$/, loader: "jsx" },
+      <% } else { %>
+      { test: /\.coffee$/, loader: "coffee!cjsx" },
+      <% } %>
 
-            <% if ( stylesheetSyntax === 'CSS' ) { %>
-            { test: /\.css$/, loader: "style!css" },
-            <% } else if ( stylesheetSyntax === 'SCSS') { %>
-            {
-              test: /\.scss$/,
-              loader: "style!css!sass?outputStyle=expanded"
-            },
-            <% } else if ( stylesheetSyntax === 'LESS') { %>
-            {
-              test: /\.less$/,
-              loader: "style!css!less"
-            },
-            <% } else { %>
-            {
-              test: /\.sass$/,
-              loader: "style!css!sass?indentedSyntax=true&outputStyle=expanded"
-            },
-            <% } %>
+      <% if ( stylesheetSyntax === 'CSS' ) { %>
+      { test: /\.css$/, loader: "style!css" },
+      <% } else if ( stylesheetSyntax === 'SCSS') { %>
+      {
+        test: /\.scss$/,
+        loader: "style!css!sass?outputStyle=expanded"
+      },
+      <% } else if ( stylesheetSyntax === 'LESS') { %>
+      {
+        test: /\.less$/,
+        loader: "style!css!less"
+      },
+      <% } else { %>
+      {
+        test: /\.sass$/,
+        loader: "style!css!sass?indentedSyntax=true&outputStyle=expanded"
+      },
+      <% } %>
 
-            {
-              test: /\.(html|png)$/,
-              loader: "file?name=[path][name].[ext]&context=./src"
-            }
-        ]
-    }
+      {
+        test: /\.(html|png)$/,
+        loader: "file?name=[path][name].[ext]&context=./src"
+      }
+    ]
+  }
 };
-
